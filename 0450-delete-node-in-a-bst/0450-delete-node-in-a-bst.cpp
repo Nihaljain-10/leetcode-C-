5a76230a9ae6ea5223f1429549491236426bb1bb
Nihal Jain TP <114482401+Nihaljain-10@users.noreply.github.com>
2022-10-16 23:50:36 +0530

    Time: 78 ms (18.60%), Space: 32.5 MB (97.23%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(!root)
+                return NULL;
+            if(root->val==key)
+                    return helper(root);
+             TreeNode* dummy=root;
+            while(root){
+                    if(root->val>key){
+                            if(root->left!=NULL && root->left->val==key){
+                                    root->left=helper(root->left);
+                                    break;
+                            }
+                            else{
+                                    root=root->left;
+                            }
+                    }
+                    else{
+                       if(root->right!=NULL && root->right->val==key){
+                                    root->right=helper(root->right);
+                                    break;
+                            }
+                            else{
+                                    root=root->right;
+                            }     
+                    }  
+            }
+            return dummy;
+    }
+       TreeNode* helper(TreeNode*root){
+               if(root->left==NULL)
+                       return root->right;
+               if(root->right==NULL)
+                       return root->left;
+                TreeNode* rightchild=root->right;
+                TreeNode*lastright=findlastright(root->left);
+               lastright->right=rightchild;
+               return root->left;
+       }
+         TreeNode* findlastright( TreeNode*root){
+                 if(root->right==NULL)
+                         return root;
+                 return findlastright(root->right);
+         }
+};
