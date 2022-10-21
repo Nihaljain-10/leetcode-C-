0d1106bb9875175b76851ee91d5680a3af4b0fb8
Nihal Jain TP <114482401+Nihaljain-10@users.noreply.github.com>
2022-10-21 15:41:55 +0530

    Time: 184 ms (18.92%), Space: 27.5 MB (70.08%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,41 +1,48 @@
 class Solution {
 public:
-    int numEnclaves(vector<vector<int>>& grid){
-  int m = grid.size();
-  int n = grid[0].size();
-  int count = 0;
-  for (int i = 0; i < m; i++) {
-    for (int j = 0; j < n; j++) {
-      if (grid[i][j] == 1) {
-        count++;
-      }
+    int numEnclaves(vector<vector<int>>& grid) {
+         queue<pair<int,int>> q; 
+        int n = grid.size(); 
+        int m = grid[0].size(); 
+        vector<vector<int>>vis(n,vector<int>(m,0)); 
+        for(int i = 0;i<n;i++) {
+            for(int j = 0;j<m;j++) {
+                if(i == 0 || j == 0 || i == n-1 || j == m-1) {
+        
+                    if(grid[i][j] == 1) {
+                        q.push({i, j}); 
+                        vis[i][j] = 1; 
+                    }
+                }
+            }
+        }
+        
+        int delrow[] = {-1, 0, +1, 0};
+        int delcol[] = {0, +1, +0, -1}; 
+        
+        while(!q.empty()) {
+            int row = q.front().first; 
+            int col = q.front().second; 
+            q.pop(); 
+            for(int i = 0;i<4;i++) {
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i]; 
+                if(nrow >=0 && nrow <n && ncol >=0 && ncol < m 
+                && vis[nrow][ncol] == 0 && grid[nrow][ncol] == 1) {
+                    q.push({nrow, ncol});
+                    vis[nrow][ncol] = 1; 
+                }
+            }
+            
+        }
+        
+        int cnt = 0;
+        for(int i = 0;i<n;i++) {
+            for(int j = 0;j<m;j++) {
+                if(grid[i][j] == 1 & vis[i][j] == 0) 
+                    cnt++; 
+            }
+        }
+        return cnt; 
     }
-  }
-  for (int i = 0; i < m; i++) {
-    if (grid[i][0] == 1) {
-      count -= dfs(grid, i, 0);
-    }
-    if (grid[i][n - 1] == 1) {
-      count -= dfs(grid, i, n - 1);
-    }
-  }
-  for (int j = 0; j < n; j++) {
-    if (grid[0][j] == 1) {
-      count -= dfs(grid, 0, j);
-    }
-    if (grid[m - 1][j] == 1) {
-      count -= dfs(grid, m - 1, j);
-    }
-  }
-  return count;
-}
-
-int dfs(vector<vector<int>>& grid, int i, int j) {
-  if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] == 0) {
-    return 0;
-  }
-  grid[i][j] = 0;
-  return 1 + dfs(grid, i - 1, j) + dfs(grid, i + 1, j) + dfs(grid, i, j - 1) + dfs(grid, i, j + 1);
-}
-    
 };
